15f803e99b1014e11e09209fecccda541a849421
unknown <riasat01@outlook.com>
2023-09-25 12:43:09 +0600

    price added

--- src/pages/favourites/Favourites.jsx
+++ src/pages/favourites/Favourites.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import Phones from "../../components/phones/Phones";
+// import Phones from "../../components/phones/Phones";
 import ShowFavourites from "../../components/show-favourites/ShowFavourites";
 
 const Favourites = () => {
 
     const [favourites, setFavourites] = useState([]);
     const [notFound, setNotFound] = useState(false);
+    const [price, setPrice] = useState(0);
 
     useEffect(() => {
         const _ = JSON.parse(localStorage.getItem(`favourites`));
@@ -17,6 +18,12 @@ const Favourites = () => {
         }
     }, []);
 
+    useEffect(() => {
+        const total = favourites.reduce((preValue, currentValue) => preValue + currentValue.price, 0);
+        setPrice(total.toFixed(2));
+    }, [favourites])
+
+
     const handleDelete = () => {
         localStorage.clear();
         setFavourites([]);
@@ -26,10 +33,11 @@ const Favourites = () => {
     return (
         <>
             {/* <h1>favourites</h1> */}
-            <div className="w-fit m-auto">
-            {
-                notFound || <button onClick={handleDelete} className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white px-3 py-2">Delete all favourites</button>
-            }
+            <div className="w-fit m-auto space-y-4">
+                <p className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white px-3 py-2">Total: ${price}</p>
+                {
+                    notFound || <button onClick={handleDelete} className="bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white px-3 py-2">Delete all favourites</button>
+                }
             </div>
             <div className="">
                 {
